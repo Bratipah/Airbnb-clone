135a455785a485e463d54d92d749aaa12199a19f
Bratipah <bratipahmh@gmail.com>
2021-11-11 04:43:37 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    //BEM
+
+    <div className="app">
+      <h1>Lets build Airbnb clone</h1>
+
+      {/* Header */}
+
+      {/* Banner */}
+
+      
+    </div>
+  );
+}
+
+export default App;
