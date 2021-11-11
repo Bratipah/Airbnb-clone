e5a5b06d8ff77550918f47ca99c3f45089c6a9ad
Bratipah <bratipahmh@gmail.com>
2021-11-11 05:17:05 +0300

    add airbnb logo

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,13 @@ import "./Header.css"
 
 function Header() {
     return (
-        <div className = "header">
-            
+        <div className = 'header'>
+            <img 
+                className= "header__icon"
+                src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
+                alt =""
+
+            />
         </div>
     )
 }
