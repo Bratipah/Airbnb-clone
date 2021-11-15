fdfcfff104a9c5fd467075349721eb6e8cd0f3f9
Bratipah <bratipahmh@gmail.com>
2021-11-15 08:33:08 +0300

    add date picker

--- src/Banner.js
+++ src/Banner.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import "./Banner.css"
+import Search from "./Search"
 import { Button } from "@material-ui/core";
 
 function Banner() {
@@ -7,7 +8,7 @@ function Banner() {
     return (
         <div className = 'banner'>
             <div classname = "banner__search">
-                {showSearch && <h1>SHOW DATE PICKER</h1>}
+                {showSearch && <Search />}
                 <Button onClick = {() => setShowSearch(!showSearch)}
                 className = "banner__searchButton"
                 variant = "outlined"> Search Dates
